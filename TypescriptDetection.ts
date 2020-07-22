3b9962aee5412167f50c5bab21cf65f5e0157a9b
P.Gerdenel <47711469+pgerdenel@users.noreply.github.com>
2020-07-22 15:48:44 +0200

    Add files via upload

---
+++ TypescriptDetection.ts
@@ -0,0 +1,14 @@
+class LanguageDetection { 
+   //field 
+   engine:string; 
+ 
+   //constructor 
+   constructor(engine:string) { 
+      this.engine = engine 
+   }  
+
+   //function 
+   disp():void { 
+      console.log("Engine is  :   "+this.engine) 
+   } 
+}
